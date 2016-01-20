7239353f7e197e497b6d3a31ea3c9f8ff828ed64
Paul Ingles <paul@oobaloo.co.uk>
2016-01-20 11:09:21 +0000

    add wait option- controls how long we wait to acquire lock. if we can't acquire in time we exit

--- src/glock/main.go
+++ src/glock/main.go
@@ -13,33 +13,48 @@ var (
 	path           = kingpin.Flag("path", "zookeeper path for lock").Required().String()
 	command        = kingpin.Flag("command", "command to execute").Required().String()
 	sleep          = kingpin.Flag("sleep", "duration to sleep after task executes to ensure single execution.").Default("15s").Duration()
-	sessionTimeout = kingpin.Flag("sessionTimeout", "zookeeper session timeout.").Default("5s").Duration()
+	wait           = kingpin.Flag("wait", "duration to wait for lock before exiting.").Default("5s").Duration()
+	sessionTimeout = kingpin.Flag("sessionTimeout", "zookeeper session timeout.").Default("10s").Duration()
 )
 
 func parseZooKeeper(zkarg string) []string {
 	return strings.Split(zkarg, ",")
 }
 
+func lockChannel(zookeeper string, sessionTimeout time.Duration, path string) <-chan *lock {
+	lockCh := make(chan *lock, 1)
+	go func() {
+		zookeepers := parseZooKeeper(zookeeper)
+		lock, err := acquireLock(zookeepers, sessionTimeout, path)
+		if err != nil {
+			fmt.Println("error acquiring lock, exiting:", err.Error())
+			os.Exit(1)
+		}
+
+		if lock == nil {
+			fmt.Println("didn't acquire lock")
+			os.Exit(1)
+		}
+
+		if lock != nil {
+			lockCh <- lock
+		}
+	}()
+	return lockCh
+}
+
 func main() {
 	kingpin.Parse()
 
-	zookeepers := parseZooKeeper(*zookeeper)
-	lock, err := acquireLock(zookeepers, *sessionTimeout, *path)
-	if err != nil {
-		fmt.Println("error acquiring lock, exiting:", err.Error())
-		os.Exit(1)
-	}
+	lockCh := lockChannel(*zookeeper, *sessionTimeout, *path)
 
-	if lock != nil {
-		fmt.Println("LOCK ACQUIRED, EXECUTING")
+	select {
+	case lock := <-lockCh:
 		runCommand(parseCommand(*command))
-	} else {
-		fmt.Println("error acquiring lock, exiting")
+		time.Sleep(*sleep)
+		lock.Unlock()
+	case <-time.After(*wait):
+		fmt.Println("couldn't acquire lock in time, exiting.")
 		os.Exit(1)
 	}
-
-	fmt.Println("waiting for", *sleep)
-	<-time.After(*sleep)
-
-	lock.Unlock()
 }
